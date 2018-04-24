2b00cdaf6eca8b697ecd6158d612bbbd8cf9fbcf
Arielle Gordon <reallygordon@gmail.com>
2018-04-24 12:45:32 -0400

    added ls.js, edited prompt

--- bash.js
+++ bash.js
@@ -3,10 +3,15 @@ process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim();
   if (cmd === 'pwd') {
       pwd()
-    } else {
+  } else if (cmd === 'ls') {
+      ls()
+  } else {
       process.stdout.write('You typed: ' + cmd);
-    }
-	process.stdout.write('\nprompt > ');
+  }
+	setTimeout(
+    function() {process.stdout.write('\nprompt > ')}, 10);
+    //is there a way to make "prompt>" run after we've executed our "fs.readdir" from ls.js?
 });
 
 const pwd = require('./pwd.js');
+const ls = require('./ls.js')
